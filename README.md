9062c5b2a0710a7d82f06edacbbe3f6548bddc3b
mbasy <mby@live.se>
2023-05-10 23:59:24 +0200

    Made the program shorter and fixed some bugs, also added readme

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# topicfinder
+
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+
+  <h3 align="center">Topic-Finder project</h3>
+
+  <p align="center">
+    Here you can find how to use the application/API
+  </p>
+</div>
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+This is an example of how you can use the application locally.
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+This is an example of how to list things you need to use the software and how to install them.
+* npm
+  ```sh
+  npm install npm@latest -g
+  ```
+
+### Installation
+
+_Below is an example of how you can instruct your audience on installing and setting up your app. This template doesn't rely on any external dependencies or services._
+
+1. Clone the repo
+2. Clone the repo
+   ```sh
+   git clone https://github.com/mbasy/topicfinder.git
+   ```
+3. Install NPM packages
+   ```sh
+   npm install
+   ```
+4. Enter your API in `config.js`
+   ```js
+   const API_KEY = 'ENTER YOUR API';
+   ```
+
--- api.py
+++ api.py
@@ -2,9 +2,7 @@ from datetime import datetime
 start_time = datetime.now()
 import pandas as pd
 import re
-import sys
-import platform 
-import argparse
+import os
 from distutils.log import debug
 from fileinput import filename
 from flask import *  
@@ -19,7 +17,6 @@ def get_topic(file1, file2)->list:
     my_series = pd.Series(sheet_to_df_map)
 
     cat_file = my_series[my_series.notna()].to_dict()
-    #cat_file = cat_file.apply(lambda s: s.dropna().astype(str).tolist()).to_dict()
     text_file = pd.read_excel(file2)
     text_file = text_file['Ärende'].to_list()
 
@@ -27,63 +24,40 @@ def get_topic(file1, file2)->list:
         return list(dictionary.keys())
 
     cat_list = get_dict_key(cat_file)
-    #print(cat_list)
 
-
-    index_counter = 0
-
-    parser = argparse.ArgumentParser(description="Script")
-    parser.add_argument("--text")
-    args, leftovers = parser.parse_known_args()
-
-    print("Arg has been set (value is {0})".format(args.text))
     s_text = text_file
-    print('\n')
 
+    os.remove(file1)
+    os.remove(file2) 
+	
     word_counter = 0
 
     cat_result = []
     result_list = []
     for n_of_text in s_text:
-        print("Reading text:",word_counter+1)
         for t in cat_list:
             r = re.findall(r'|'.join(cat_file[t]), n_of_text, re.IGNORECASE)
             if(r):
-            # print(f"{bcolors.OKCYAN}Topic found(",t,"):",r, bcolors.ENDC)
                 if(t in cat_result):
                     result_list[cat_result.index(t)] = result_list[cat_result.index(t)]+len(r)
                 else:
                     cat_result.append(t)
                     result_list.append(len(r))
-                    #print("\t","Number of words found:", word_counter, "Total words in text file:", number_of_word_in_text,
-                # round((word_counter/number_of_word_in_text)*100, 2), "%")
         word_counter += 1
-
-    print(cat_result)
-    print(result_list)
     html_list = []
     loop_counter = 0
     total_value = sum(result_list)
     for cat in cat_result:
-        #print(f"Topic found(",cat_result[loop_counter],"):"
-        #    , result_list[loop_counter],"Total:", round((result_list[loop_counter]/total_value)*100, 2), "%")
-        dt = f"{cat_result[loop_counter]} {round((result_list[loop_counter]/total_value)*100, 2)}%"
+        dt = f"{cat_result[loop_counter]} : {round((result_list[loop_counter]/total_value)*100, 2)}%"
         html_list.append(dt)
         loop_counter+=1
     return(html_list)
-
-def print_ul(elements):
-    print("<ul>")
-    for s in elements:
-        ul = "<li>" + str(s) + "</li>"
-        print(ul)
-    print("</ul>")
 
 @app.route('/')  
 def main():  
     return render_template("index.html")  
   
-@app.route('/success', methods = ['POST'])  
+@app.route('/topic_api', methods = ['POST'])  
 
 def success(): 
     if request.method == 'POST':  
@@ -93,5 +67,17 @@ def success():
         f2.save(f2.filename)
         return render_template("Acknowledgement.html", name1 = f1.filename, name2 = f2.filename,
                                result = get_topic(f1.filename, f2.filename))  
+
+@app.route('/topic_api_json', methods = ['POST']) 
+
+def api_only():
+    if request.method == 'POST':  
+        f1 = request.files['file1']
+        f2 = request.files['file2']
+        f1.save(f1.filename)
+        f2.save(f2.filename)
+        result = get_topic(f1.filename, f2.filename)
+        return result
+
 if __name__ == '__main__':  
     app.run(host="0.0.0.0")
